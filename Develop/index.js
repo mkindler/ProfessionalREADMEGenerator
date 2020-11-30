8f74efe000a823828b38176dc28f8a2c54ce2f80
Mae Kindler <mae.akie.kindler@gmail.com>
2020-11-30 15:58:45 -0600

    Determined required npm packages for installation. Began writing user questions for the creation of the README file.

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,32 @@
+// require specific npm packages
+const fs = require("fs");
+const util = require("util");
+const path = require("path");
+const inquirer = require("inquirer");
+
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project:"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Describe the installation process of dependencies for this project:"
+    }
+    {
+        type: "list",
+        name: "license",
+        message: "What kind of license should your project have?",
+        choices: ["Apache", "BSD", "GPL", "MIT", "Other", "None"]
+    },
 
 ];
 
