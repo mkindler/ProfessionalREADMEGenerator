b4424649f6a49d7466543a278ea44378bff2d953
Mae Kindler <mae.akie.kindler@gmail.com>
2020-11-30 17:16:26 -0600

    Created a function to display the chosen license badge, as well as a message that will display if license information is not available.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,17 @@
+// function to create appropriate license badge.  If license is listed as "Other" or "None", a message will be displayed stating that license information is not available.
+function createLicenseBadge(license) {
+  if (license !== "Other" || license !== "None") {
+    return `![GitHub License](https://img.shields.io/badge/license-${license}-blue.svg)`
+  }
+  return "Licensing information for this application is not available."
+}
+
+// function to create a section for information about licensing.  If license is listed as "Other" or "None", a message will be displayed in the section stating that license information is not available.
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${createLicenseBadge(data.license)}
 
 ## Description
 
@@ -11,6 +22,8 @@ ${data.description}
 * [Installation Instructions](#installation)
 
 * [Usage Information](#usage)
+
+* [License](#license)
 
 * [Contributing](#contributing)
 
@@ -27,6 +40,12 @@ ${data.installation}
 ## Usage Information
 
 ${data.usage}
+
+## License
+
+![badge](https://img.shields.io/badge/license-${license}-blue.svg)
+<br />
+This application is covered by the ${data.license} license.
 
 ## Contributing
 
