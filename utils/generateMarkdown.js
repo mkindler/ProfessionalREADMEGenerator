3cb7ec49502e04529b0da2b38c4ea788c0308a1f
Mae Kindler <mae.akie.kindler@gmail.com>
2020-11-30 16:54:12 -0600

    Created basic structure of README file template in the generateMarkdown.js file.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,48 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+
+${data.description}
+
+## Table of Contents
+
+* [Installation Instructions](#installation)
+
+* [Usage Information](#usage)
+
+* [Contributing](#contributing)
+
+* [Testing](#testing)
+
+* [Questions?](#questions)
+
+## Installation Instructions
+
+To install necessary dependencies, run the following command:
+
+${data.installation}
+
+## Usage Information
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## Testing
+
+To run tests, run the following command:
+
+${data.testing}
+
+## Questions?
+
+If you have any questions about this repository, please open an issue or contact me directly at ${data.email}.
+
+Looking for more of my work?  Please visit [${data.username}](https://github.com/${data.username}/).
+
 `;
 }
 
