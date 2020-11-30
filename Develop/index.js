c56785b78666632167feb559c82a0d5d1727b3b0
Mae Kindler <mae.akie.kindler@gmail.com>
2020-11-30 16:10:40 -0600

    Completed list of prompt questions.

--- Develop/index.js
+++ Develop/index.js
@@ -19,13 +19,38 @@ const questions = [
     {
         type: "input",
         name: "installation",
-        message: "Describe the installation process of dependencies for this project:"
-    }
+        message: "Describe the installation process of dependencies for this project. The typical command used is npm i."
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What does the user need to know about using the repository?"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "What does the user need to know about contributing to the repository?"
+    },
+    {
+        type: "input",
+        name: "testing",
+        message: "What command is needed to run testing? The typical command used is npm test."
+    },
     {
         type: "list",
         name: "license",
         message: "What kind of license should your project have?",
         choices: ["Apache", "BSD", "GPL", "MIT", "Other", "None"]
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
     },
 
 ];
